05bb1455f8109a1aa3b48985f71e7fa864acd115
Ofir Ron <ofir@msapps.mobi>
2017-05-14 20:52:35 +0300

    Add example files for how to use the project

--- Example/MyCocoView/ViewController.swift
+++ Example/MyCocoView/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import MyCocoView
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cocoView: MyCocoView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,5 +24,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func cocoPressed(_ sender: Any) {
+        cocoView.coco()
+    }
 }
 
